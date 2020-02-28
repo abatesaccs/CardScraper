96dc5b6d19fe3bda5ff0ad867138c24fe6eb12d2
Andrew Bates <ABatesAccs@gmail.com>
2020-02-28 15:21:19 -0800

    seperate card information

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -8,19 +8,28 @@ def Scrape():
     card_page = requests.get('https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=416938')
     tree = html.fromstring(card_page.content)
 
-    card_details = tree.xpath('//*[@id="ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_rightCol"]/div[2]')
+    card_details = tree.xpath('//*[@id="ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_rightCol"]/div[2]/node()')
+
+    print(len(card_details))
 
     temp = ''
+    
+    # temp = Format(card_details[7].text_content())
     for i in range(len(card_details)):
         if(str(card_details[i]).find('Element') != -1):
-            temp += card_details[i].text_content()
+            temp += Format(card_details[i].text_content()) + '\n'
+    
+    tempList = temp.split('\n')
+    # tempList = tempList.remove(u'')
+    print(temp + '\n\n\n')
+    print(tempList)
 
-    tempText = Format(temp)
-    print(tempText)
-
-    f = open('text.txt', 'w+')
-    f.write(tempText.encode('utf8'))
-    f.close()
+    for i in range(len(tempList)):
+        print(tempList[i])
+ 
+    # f = open('text.txt', 'w+')
+    # f.write(tempText.encode('utf8'))
+    # f.close()
 
 def Format(str):
     return re.sub(' +',' ',str.strip().replace('\r\n',''))
