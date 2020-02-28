ca1b88acead58b8faf448518ad310a5f87f91338
Andrew Bates <ABatesAccs@gmail.com>
2020-02-28 14:26:54 -0800

    save card information to text file

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -8,20 +8,19 @@ def Scrape():
     card_page = requests.get('https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=416938')
     tree = html.fromstring(card_page.content)
 
-    small_grey = tree.xpath('//*[@id="ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_rightCol"]/div[2]')
+    card_details = tree.xpath('//*[@id="ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_rightCol"]/div[2]')
 
     temp = ''
-    for i in range(len(small_grey)):
-        if(str(small_grey[i]).find('Element') != -1):
-            temp += small_grey[i].text_content()
+    for i in range(len(card_details)):
+        if(str(card_details[i]).find('Element') != -1):
+            temp += card_details[i].text_content()
 
-    # tempText = ''
-    # for i in range(len(temp)):
-    #     tempText += temp[i].text_content().strip() + '|||'
-    # tempText = tempText.replace('\r\n', '')
-    # tempText = re.sub(' +',' ',tempText)
     tempText = Format(temp)
     print(tempText)
+
+    f = open('text.txt', 'w+')
+    f.write(tempText.encode('utf8'))
+    f.close()
 
 def Format(str):
     return re.sub(' +',' ',str.strip().replace('\r\n',''))
