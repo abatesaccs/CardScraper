0e7c0a7a463ddeb350b9328c0b4edcd117215ae3
Andrew Bates <ABatesAccs@gmail.com>
2020-02-28 15:21:32 -0800

    format card information

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -5,40 +5,26 @@ from lxml import etree
 import sched, time
 
 def Scrape():
-    card_page = requests.get('https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=416938')
+    card_page = requests.get('https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=3')
     tree = html.fromstring(card_page.content)
 
     card_details = tree.xpath('//*[@id="ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_rightCol"]/div[2]/node()')
 
-    print(len(card_details))
-
     temp = ''
-    
-    # temp = Format(card_details[7].text_content())
     for i in range(len(card_details)):
         if(str(card_details[i]).find('Element') != -1):
             temp += Format(card_details[i].text_content()) + '\n'
     
     tempList = temp.split('\n')
-    # tempList = tempList.remove(u'')
-    print(temp + '\n\n\n')
-    print(tempList)
 
     for i in range(len(tempList)):
         print(tempList[i])
  
-    # f = open('text.txt', 'w+')
-    # f.write(tempText.encode('utf8'))
-    # f.close()
+    f = open('text.txt', 'a+')
+    f.write(temp.encode('utf8'))
+    f.close()
 
 def Format(str):
     return re.sub(' +',' ',str.strip().replace('\r\n',''))
 
 Scrape()
-
-# formatted = title[0].strip()
-# print(tree)
-# print(small_grey)
-# print(title[0])
-# print(title)
-# print(formatted)
