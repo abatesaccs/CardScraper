2e81f39689e5287191065140e9b8a1fb92941ed2
Andrew Bates <ABatesAccs@gmail.com>
2020-02-28 14:12:45 -0800

    format card data, kinda

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,31 +2,33 @@ import requests
 import re
 from lxml import html
 from lxml import etree
+import sched, time
 
-card_page = requests.get('https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=416938')
-tree = html.fromstring(card_page.content)
+def Scrape():
+    card_page = requests.get('https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=416938')
+    tree = html.fromstring(card_page.content)
 
-title = tree.xpath('//*[@id="ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_nameRow"]/div[2]/text()')
-cmc = tree.xpath('//*[@id="ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cmcRow"]/div[2]/text()')
+    small_grey = tree.xpath('//*[@id="ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_rightCol"]/div[2]')
 
-small_grey = tree.xpath('//*[@id="ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_rightCol"]/div[2]')
+    temp = ''
+    for i in range(len(small_grey)):
+        if(str(small_grey[i]).find('Element') != -1):
+            temp += small_grey[i].text_content()
 
+    # tempText = ''
+    # for i in range(len(temp)):
+    #     tempText += temp[i].text_content().strip() + '|||'
+    # tempText = tempText.replace('\r\n', '')
+    # tempText = re.sub(' +',' ',tempText)
+    tempText = Format(temp)
+    print(tempText)
 
-formatted = title[0].strip()
+def Format(str):
+    return re.sub(' +',' ',str.strip().replace('\r\n',''))
 
-temp = []
-for i in range(len(small_grey)):
-    if(str(small_grey[i]).find('Element') != -1):
-        temp += small_grey[i]
+Scrape()
 
-
-tempText = ''
-for i in range(len(temp)):
-    tempText += temp[i].text_content().strip() + '|||'
-# tempText = tempText.replace('\r\n                                            ', '')
-tempText = re.sub(' +',' ',tempText)
-print(tempText)
-
+# formatted = title[0].strip()
 # print(tree)
 # print(small_grey)
 # print(title[0])
